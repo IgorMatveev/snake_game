79e4b432a3f9d8a43cfd25dec64cac5d616e984a
Igor Matveev <igor.matveev@phystech.edu>
2019-11-05 22:44:51 +0300

    Связи между экземплярами классов, класс FIFO, ipynb->py
    - В конструкторы классов добавлены ссылки на экземпляры других классов, чтобы
    иметь возможность обращаться к ним из кода методов.
    - Создан класс FIFO, экземпляр которого будет хранить и обслуживать координаты
    квадратов, занимаемых змейкой.
    - В ноутбуке настроил алиасы для сохранения кода из ячеек в файл. Это удобно,
    так как что-то меняешь в ноутбуке, тут же тестишь, в конце пробежался по
    нужным ячейкам и они записались в файл.
    - Также написал код, который будет обновлять requirements.txt, этот код нужно
    вставлять отдельной ячейкой после каждого импорта. Логика в следующем: нужно
    импортнуть новую библиотеку, ты её устанавливаешь через pip, и потом забываешь
    сделать pip freeze. Этот код решает проблему, если его вставлять после импорта

--- run_snake.py
+++ run_snake.py
@@ -6,11 +6,12 @@ class World():
     """Мир, в котором существует змейка.
 Представляет из себя разбитую на квадраты облать окна (карты). В нём
 могут существовать змейки, есть генератор еды для змеек."""
+    
     rows = 20
     width = 500
     objs = []
     
-    def __init__(self):
+    def __init__(self, game):
         """Создает мир"""
         pass
     
@@ -21,10 +22,33 @@ class World():
 
 class Snake():
     """Змейка
-Перемещается, ест еду. Если кусает своё тело или хвост, то умирает."""
-    def __init__(self):
+Перемещается, ест еду. Если кусает своё тело или хвост, то умирает.
+Хранит своё состояние в FIFO-очереди, в которой лежат относительные координаты квадтратов мира."""   
+    
+    def __init__(self, game, world, start_coords):
         """Создает змейку"""
+        self.game = game
+        self.world = world
+        self.coords = type(self).FIFO(start_coords)
         pass
+    
+    class FIFO():
+        """FIFO-очередь
+Хранит соординаты положения змейки"""
+        
+        def __init__(self, default=None):
+            self.queue = [default] if default else []
+        
+        def put(self, value):
+            for i in range(len(self.queue)-1):
+                self.queue[i] = self.queue[i+1]
+            self.queue[-1] = value
+        
+        def expand(self, value):
+            self.queue.append(value)
+            
+        def __repr__(self):
+            return self.queue.__repr__()
     
     def draw(self):
         """Рисует змейку"""
@@ -37,6 +61,7 @@ class Snake():
     def selfcheck(self):
         """Проверяет своё состояние"""
         pass
+    
 
 
 class Game():
